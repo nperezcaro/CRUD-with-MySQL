03ca7f4ea6c45a79c85103dd601031b04a3eed35
Nicolas Perez <nicolas.perez.caro@outlook.com>
2022-05-06 10:23:57 -0500

    Add an except to show any error

--- DB/connection.py
+++ DB/connection.py
@@ -1,9 +1,27 @@
 import mysql.connector
+from mysql.connector import Error
 
 
 class DAO():
 
     def __init__(self):
         try:
-            self.connection=mysql.connector.connect()
-        except:
+            self.connection=mysql.connector.connect(
+                host = 'localhost',
+                port = 3306,
+                user = 'root',
+                password = 'Tobby2019',
+                db = 'portfolio'
+            )
+        except Error as ex:
+            print("Error al intentar la conexión: {a}".format(ex))
+
+    def ShowAssets(self):
+        if self.connection.is_connected():
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute("SELECT  * FROM assets")
+                results = cursor.fetchall()
+                return results
+            except Error as ex:
+                print("Error al intentar la conexión: {a}".format(ex))
