8aea73c6b7201fac3186df2ac45669b767c962ec
Nicolas Perez <nicolas.perez.caro@outlook.com>
2022-05-06 10:22:03 -0500

    Update the menu: add a whyle cycle for better functioning

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 
 def PrincipalMenu():
-    print("------PRINCIPAL MENU---------")
-    print("1 - Show Assets")
-    print("2 - Buy Assets")
-    print("3 - Update Assets")
-    print("4 - Sell Assets")
-    print("5 - Exit")
-    print("------------------------------")
-    option = int(input("Select an option: "))
+    continue_ = True
+    while(continue_):
+        correctOption = False
+        while(not correctOption):
+            print("------PRINCIPAL MENU---------")
+            print("1 - Show Assets")
+            print("2 - Buy Assets")
+            print("3 - Update Assets")
+            print("4 - Sell Assets")
+            print("5 - Exit")
+            print("------------------------------")
+            option = int(input("Select an option: "))
 
-    if option <1 or option>5:
-        print("Option selected doesn't exist, please select a correct option.")
-    elif option == 5:
-        print("Thanks for using this system")
-    else:
-        executeOption(option)
+            if option <1 or option>5:
+                print("Option selected doesn't exist, please select a correct option.")
+            elif option == 5:
+                continue_ = False
+                print("Thanks for using this system")
+                break
+            else:
+                correctOption = True
+                executeOption(option)
 
 
 def executeOption(option):
