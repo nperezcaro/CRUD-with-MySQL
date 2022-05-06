bc3ee606113de06cf9625c6f65d28a0e51ae0097
Nicolas Perez <nicolas.perez.caro@outlook.com>
2022-05-06 10:39:21 -0500

    Create the function to store the data of the DB and feed it to main.py file

---
+++ functions.py
@@ -0,0 +1,8 @@
+def showAssets(assets):
+    print("Assets: ")
+    counter = 1
+    for asset in assets:
+        data = "{0}. Ticker: {1} | Quantity {2}"
+        print(data.format(counter, asset[0], asset[1]))
+        counter += 1
+    print(" ")
